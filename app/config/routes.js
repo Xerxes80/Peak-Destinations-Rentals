3acb45a194d7c2e78e62dad8de753e1eabd8f217
Kevin <kevin.tabesh@gmail.com>
2017-09-29 23:24:10 -0500

    Peak Destination Rentals

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var router = require("react-router");
+
+var Route = router.Route;
+
+var IndexRoute = router.IndexRoute;
+
+var Router = router.Router;
+
+module.exports = ();
